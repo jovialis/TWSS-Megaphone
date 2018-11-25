8a802cfad70908acab2b19c4eeec8b70f0939d24
Dylan Hanson <cdh.create@gmail.com>
2018-11-25 14:19:07 -0500

    Initial redesign. Built new splash page in raw HTML under design/

--- scripts/site.js
+++ scripts/site.js
@@ -7,20 +7,20 @@
  */
 
 (function() {
-  'use strict';
+    'use strict';
 
-  // Load all images via Squarespace's Responsive ImageLoader
-  function loadAllImages() {
-    var images = document.querySelectorAll('img[data-src]' );
-    for (var i = 0; i < images.length; i++) {
-      ImageLoader.load(images[i], {load: true});
+    // Load all images via Squarespace's Responsive ImageLoader
+    function loadAllImages() {
+        var images = document.querySelectorAll('img[data-src]' );
+        for (var i = 0; i < images.length; i++) {
+            ImageLoader.load(images[i], {load: true});
+        }
     }
-  }
 
-  // The event subscription that loads images when the page is ready
-  document.addEventListener('DOMContentLoaded', loadAllImages);
+    // The event subscription that loads images when the page is ready
+    document.addEventListener('DOMContentLoaded', loadAllImages);
 
-  // The event subscription that reloads images on resize
-  window.addEventListener('resize', loadAllImages);
+    // The event subscription that reloads images on resize
+    window.addEventListener('resize', loadAllImages);
 
 }());
